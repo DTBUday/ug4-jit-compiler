3f5224fdb3f83d53e816a1da0423acb8d389b020
Uday Patel <udaykpatel@hotmail.com>
2020-11-20 03:30:16 +0000

    Fix script

--- scripts/data_visuals.py
+++ scripts/data_visuals.py
@@ -6,17 +6,21 @@ gcc_runtime = []
 tcc_runtime = []
 
 with open('summary.csv') as csvfile:
-	file = csv.reader(csvfile, delimiter = ',')
-	for row in file:
-		# 'benchmark', 'loc', 'gcc_usr', 'gcc_sys', 'gcc_wall', 'gcc_ggc', 'tcc'
-		benchmark_name, loc, gcc_usr, gcc_sys, gcc_wall, gcc_ggc, tcc = row
+    file = csv.reader(csvfile, delimiter = ',')
+    for row in file:
+        benchmark_name, loc, gcc_usr, gcc_sys, gcc_wall, gcc_ggc, tcc = row
 
-		labels.append(benchmark_name)
-		gcc_runtime.append(gcc)
-		tcc_runtime.append(tcc)
+        if float(tcc) == 0:
+            continue
+
+        labels.append(benchmark_name)
+        gcc_runtime.append(gcc_wall)
+        tcc_runtime.append(tcc)
 
 fig, ax = plt.subplots()
 
-ax.scatter(gcc, tcc)
-ax.set_xlabel(gcc)
-ax.set_ylabel(tcc)
+ax.scatter(gcc_runtime, tcc_runtime)
+ax.set_xlabel('gcc')
+ax.set_ylabel('tcc')
+plt.show()
+
