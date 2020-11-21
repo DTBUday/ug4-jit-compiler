19f00523a5c2c44d1b243f3152dd079d7dbcaf64
Uday Patel <udaykpatel@hotmail.com>
2020-11-21 17:03:53 +0000

    Update graphing methods

--- scripts/data_visuals.py
+++ scripts/data_visuals.py
@@ -10,17 +10,17 @@ with open('summary.csv') as csvfile:
     for row in file:
         benchmark_name, loc, gcc_usr, gcc_sys, gcc_wall, gcc_ggc, tcc = row
 
-        if float(tcc) == 0:
+        if float(tcc) == 0.0:
+            print(f"TCC FAILED: {benchmark_name}")
             continue
 
         labels.append(benchmark_name)
-        gcc_runtime.append(gcc_wall)
-        tcc_runtime.append(tcc)
+        gcc_runtime.append(float(gcc_wall))
+        tcc_runtime.append(float(tcc))
 
-fig, ax = plt.subplots()
-
-ax.scatter(gcc_runtime, tcc_runtime)
-ax.set_xlabel('gcc')
-ax.set_ylabel('tcc')
+plt.plot(gcc_runtime, 'r--')
+plt.plot(tcc_runtime, 'b.')
 plt.show()
 
+plt.scatter(gcc_runtime, tcc_runtime)
+plt.show()
