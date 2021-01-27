89abae9cfcfdfd76b65c9888eb43132835dbf627
Uday Patel <udaykpatel@hotmail.com>
2021-01-27 23:22:09 +0000

    Update runtime graphing and graph

--- runtime_graphing.py
+++ runtime_graphing.py
@@ -14,20 +14,24 @@ flag = False
 for line in sys.stdin:
 	line = int(line)
 	if flag == True:
-		if temp <= 1e6:
-			gcc_runtime.append(line)
-			runtime_pairs.append((temp, line))
+		gcc_runtime.append(line)
+		runtime_pairs.append((temp, line))
 	else:
 		temp = line
-		if temp <= 1e6:
-			jit_runtime.append(line)
+		jit_runtime.append(line)
 
 	flag = not flag
-
-	if len(runtime_pairs) >= 30:
-		break
 
 x = np.linspace(0, 500000, 1000)
 plt.plot(x, x, 'r-')
 plt.scatter(gcc_runtime, jit_runtime)
+plt.gca().set_xscale('log')
+plt.gca().set_yscale('log')
+
+i = 0
+for x,y in zip(gcc_runtime, jit_runtime):
+    label = "{:.2f}".format(i)
+    plt.annotate(label, (x,y), textcoords="offset points", xytext=(0,10), ha='center')
+    i+=1
+
 plt.show()
