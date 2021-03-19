59d67652f3647c51c6eee00317b5fc838270bbe7
Uday Patel <udaykpatel@hotmail.com>
2021-03-19 15:16:14 +0000

    Update graph for runtime comparison

--- runtime_graphing.py
+++ runtime_graphing.py
@@ -12,26 +12,31 @@ jit_runtime = []
 
 flag = False
 for line in sys.stdin:
-	line = int(line)
-	if flag == True:
-		gcc_runtime.append(line)
-		runtime_pairs.append((temp, line))
-	else:
-		temp = line
-		jit_runtime.append(line)
+        line = float(int(line)/1e6)
+        if flag == True:
+                gcc_runtime.append(line)
+                runtime_pairs.append((temp, line))
+        else:
+                temp = line
+                jit_runtime.append(line)
 
-	flag = not flag
+        flag = not flag
 
-x = np.linspace(0, 500000, 1000)
+# Log-log scaling
+x = np.linspace(0, 1, 1000)
 plt.plot(x, x, 'r-')
 plt.scatter(gcc_runtime, jit_runtime)
 plt.gca().set_xscale('log')
 plt.gca().set_yscale('log')
 
-i = 0
-for x,y in zip(gcc_runtime, jit_runtime):
-    label = "{:.2f}".format(i)
-    plt.annotate(label, (x,y), textcoords="offset points", xytext=(0,10), ha='center')
-    i+=1
+# DEBUGGING
+#i = 0
+#for x,y in zip(gcc_runtime, jit_runtime):
+#    label = "{:.2f}".format(i)
+#    plt.annotate(label, (x,y), textcoords="offset points", xytext=(0,10), ha='center')
+#    i+=1
 
+plt.title('Runtime comparison between GCC & JIT compiler')
+plt.xlabel('GCC runtime execution, seconds')
+plt.ylabel('JIT runtime execution, seconds')
 plt.show()
